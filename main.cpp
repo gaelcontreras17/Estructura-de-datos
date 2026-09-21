1fe66bec64ee7edbff33893346de39f71d015caf
Gael <ux25ii296@ux.edu.mx>
2026-09-21 16:39:44 +0000

    feat: agrega seguimiento de pasadas y contadores

--- main.cpp
+++ main.cpp
@@ -1,17 +1,30 @@
 #include <iostream>
 using namespace std;
 
-// Ordena de forma ascendente el arreglo "numeros" de tamanio n
+int comparaciones = 0;
+int intercambios = 0;
+
+// Ordena de forma ascendente el arreglo "numeros" de tamanio n,
+// mostrando el arreglo despues de cada pasada y contando
+// el numero de comparaciones e intercambios realizados.
 void burbuja(int numeros[], int n) {
     for (int i = 0; i < n - 1; i++) {
         for (int j = 0; j < n - 1 - i; j++) {
+            comparaciones++;
             if (numeros[j] > numeros[j + 1]) {
                 // Intercambio de valores en el arreglo
                 int temporal = numeros[j];
                 numeros[j] = numeros[j + 1];
                 numeros[j + 1] = temporal;
+                intercambios++;
             }
         }
+
+        cout << "Pasada " << (i + 1) << ": ";
+        for (int k = 0; k < n; k++) {
+            cout << numeros[k] << " ";
+        }
+        cout << endl;
     }
 }
 
@@ -28,11 +41,17 @@ int main() {
 
     cout << "Arreglo original: ";
     mostrarArreglo(numeros, n);
+    cout << endl;
 
     burbuja(numeros, n);
 
-    cout << "Arreglo ordenado: ";
+    cout << endl << "Arreglo ordenado: ";
     mostrarArreglo(numeros, n);
+
+    cout << endl;
+    cout << "Elementos: " << n << endl;
+    cout << "Comparaciones: " << comparaciones << endl;
+    cout << "Intercambios: " << intercambios << endl;
 
     return 0;
 }
